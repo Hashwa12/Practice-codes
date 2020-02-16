8739c81a27d7eef8fb761a3b8ca43d16e6d51459
hashwa12 <hashwanthrjn@gmail.com>
2020-02-16 13:21:12 +0530

    newly practiced code

--- src/strings/Reverse.java
+++ src/strings/Reverse.java
@@ -4,40 +4,37 @@ import java.util.Scanner;
 
 public class Reverse {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Enter the string to reverse:");
 		Scanner in = new Scanner(System.in);
 		String str = in.nextLine();
-		
+
 		String reverse = "";
-		String [] arr = str.split(" ");
-		
-		for(int i=0;i<arr.length;i++)
-		{
+		String[] arr = str.split(" ");
+
+		for (int i = 0; i < arr.length; i++) {
 			StringBuilder sb = new StringBuilder(arr[i]);
-			reverse+=sb.reverse()+" ";
+			reverse += sb.reverse() + " ";
 		}
-		System.out.println(reverse+"\t");
-			
-		for(int i=0;i<=arr.length-1;i++)
-		{
+		System.out.println(reverse + "\t");
+
+		for (int i = 0; i <= arr.length - 1; i++) {
 			reverse = "";
 			String temp = arr[i];
-			for(int j=temp.length()-1;j>=0;j--)
-			{
-				reverse+=temp.charAt(j);
+			for (int j = temp.length() - 1; j >= 0; j--) {
+				reverse += temp.charAt(j);
 			}
-			System.out.print(reverse+"\t");
+			System.out.print(reverse + "\t");
 		}
-		
-		for (int k = arr.length-1; k >=0; k--) {
-			reverse="";
+
+		for (int k = arr.length - 1; k >= 0; k--) {
+			reverse = "";
 			String temp = arr[k];
-			for (int l = temp.length()-1; l >=0; l--) {
-				reverse+=temp.charAt(l);
-			}	System.out.print(reverse+"\t");
-		}		
+			for (int l = temp.length() - 1; l >= 0; l--) {
+				reverse += temp.charAt(l);
+			}
+			System.out.print(reverse + "\t");
+		}
 	}
 
 }
