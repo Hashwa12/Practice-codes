a81f4a7871c694ee4244c958c9d217c99fba0726
Hashwa12 <50830098+Hashwa12@users.noreply.github.com>
2020-09-14 13:29:34 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package all_concepts;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		String str = "a";
+		int str1 = 97;
+		System.out.println(str.hashCode()+"\t"+str.equals(str1));
+		System.out.println("Hello world");
+	}
+
+}
